9a4675080e881c6656fde8554a3ca05ce8339d74
Kinnyeo <77890354+Kinnyeo@users.noreply.github.com>
2023-10-09 23:42:42 +0200

    Update nikss_chassis_manager.cc

--- stratum/hal/lib/nikss/nikss_chassis_manager.cc
+++ stratum/hal/lib/nikss/nikss_chassis_manager.cc
@@ -52,48 +52,6 @@ std::unique_ptr<NikssChassisManager> NikssChassisManager::CreateInstance(
     chassis_config[node_id][port.port_id] = port;
   }
 
-/*
-  // Compare ports in old config and new config and perform the necessary operations.
-  for (auto& node : config.nodes()) {
-    LOG(INFO) << "Updating config for node " << node.id() << ".";
-    for (const auto& port_old : node_id_to_port_id_to_port_config_[node.id()]) {
-      auto port_id = port_old.first;
-      auto* singleton_port = gtl::FindOrNull(
-          node_id_to_port_id_to_port_config[node.id()], port_id);
-
-      if (singleton_port == nullptr) {  // remove port if not present anymore
-        auto& config_old = port_old.second.config_params();
-        if (config_old.admin_state() == ADMIN_STATE_ENABLED) {
-          APPEND_STATUS_IF_ERROR(status, RemovePort(dev_mgr, node.id(), port_id));
-        }
-      } else {
-        auto& config_old = port_old.second.config_params();
-        auto& config = singleton_port->config_params();
-        if (config.admin_state() != config_old.admin_state()) {
-          if (config.admin_state() == ADMIN_STATE_ENABLED) {
-//            APPEND_STATUS_IF_ERROR(
-//                status,
-//                AddPort(dev_mgr, node.id(), singleton_port->name(), port_id));
-          } else {
-//            APPEND_STATUS_IF_ERROR(status,
-//                                   RemovePort(dev_mgr, node.id(), port_id));
-//            if (node_id_to_port_id_to_port_state_[node.id()][port_id] ==
-//                PORT_STATE_UP) {
-//              // TODO(antonin): would it be better to just register a bmv2
-//              // callback for PORT_REMOVED event?
-//              VLOG(1) << "Sending DOWN notification for port " << port_id
-//                      << " in node " << node.id() << ".";
-//              SendPortOperStateGnmiEvent(node.id(), port_id, PORT_STATE_DOWN);
-//            }
-          }
-        }
-      }
-    }
-
-
-  }
-  */
-
   chassis_config_ = chassis_config;
   
   return status;
