0d0f9761241d4963d2b9cca4fe3d2a6e315b9e43
Kinnyeo <77890354+Kinnyeo@users.noreply.github.com>
2023-10-09 23:40:19 +0200

    Delete script.py

--- script.py
+++
@@ -1,5 +0,0 @@
-from scapy.all import *
-from scapy.layers.inet import IP, UDP
-from scapy.layers.l2 import Dot1Q, Ether
-
-sendp(Ether(dst="00:00:00:00:01:04")/IP(dst="192.168.2.104",ttl=(1,4)), iface="veth2")
